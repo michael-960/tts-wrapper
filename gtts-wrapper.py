06cdb5193a0c182af523118a55c68358d03c6b0b
michael <mike.wang96029@gapp.nthu.edu.tw>
2023-03-01 22:15:30 +0800

    config

--- gtts-wrapper.py
+++ gtts-wrapper.py
@@ -1,9 +1,20 @@
-#!/home/michael/miniconda3/envs/tts/bin/python3
+#!/usr/bin/python3
+
 import os
+import subprocess
+import sys
+import re
+import pathlib
+import json
 
-gtts = '/home/michael/miniconda3/envs/tts/bin/gtts-cli'
 
-text = os.popen('xsel').read()
+p = pathlib.Path(__file__).resolve()
+
+with open(p / 'config.json', 'r') as f:
+    config = json.load(f)
+
+gtts = config['gtts']
+text = os.popen('xsel -b').read()
 
 subs = [
     ('-\n', ''),
@@ -21,9 +32,20 @@ subs = [
 for t, r in subs:
     text = text.replace(t, r)
 
-sentences = text.split('. ')
+# sentences = text.split('. ')
+sentences = re.split('\. |, ', text)
 
 for s in sentences:
     print(s)
-    os.system(f'echo -n "{s}" | {gtts} - | mpg123 - ')
+    p_echo = subprocess.Popen(('echo', '-n', s), stdout=subprocess.PIPE)
+    p_tts = subprocess.Popen((gtts, '-'), stdin=p_echo.stdout, stdout=subprocess.PIPE)
+    p_audio = subprocess.Popen(('mpg123', '-'), stdin=p_tts.stdout)
+    try:
+        p_audio.wait()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        p_audio.kill()
+        sys.exit(0)
+    # os.system(f'echo -n "{s}" | {gtts} - | mpg123 - ')
+
 
